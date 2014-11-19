f7d7659baec2e245c326830f65e38be4701057fb
schra_000 <schra_000@10.30.9.69>
2014-11-19 17:41:37 -0600

    INITIAL COMMIT

---
+++ src/iopractice/IOPractice.java
@@ -0,0 +1,37 @@
+package iopractice;
+
+import java.io.*;
+
+/**
+ *
+ * @author schra_000
+ */
+public class IOPractice {
+
+    public static void main(String[] args) {
+        
+        File data = new File(File.separatorChar + "temp" + File.separatorChar 
+                        + "lab1.txt");
+        
+        BufferedReader in = null;
+        try {
+	   in = new BufferedReader(new FileReader(data));  // **opening stream**
+	   String line = in.readLine();
+	   while(line != null){
+		  System.out.println(line);
+		  line = in.readLine();  // strips out any carriage return chars
+	   }
+	 
+        } catch(IOException ioe) {
+            System.out.println("Houston, we have a problem! reading this file");
+        } finally {
+            try {
+                in.close();  // **closing stream**
+            } catch(Exception e) {
+                System.out.println("Sorry could not close stream.");
+            }
+        }
+    }
+    
+    
+}
